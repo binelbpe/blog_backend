859cc306c29f817574cf35e5ce2bc8c4c4888f30
Binel Biju <binelbpe@gmail.com>
2025-01-08 21:07:18 +0530

    second

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcryptjs");
-const User = require("../models/user");
+const User = require("../models/User");
 const tokenUtils = require("../utils/tokenUtils");
 const { APIError } = require("../utils/errorHandler");
 
